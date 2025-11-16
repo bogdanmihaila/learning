2b35ceac957c4d82a4cad1eab86648caf33f03db
Bogdan Mihaila <>
2025-11-16 20:05:21 +0200

    Fix issue on android regarding status bar and back button

--- App.tsx
+++ App.tsx
@@ -1,5 +1,4 @@
 import { NavigationContainer } from "@react-navigation/native";
-import { StatusBar } from "expo-status-bar";
 import Toast from "react-native-toast-message";
 import "./i18n";
 import AppNavigator from "./navigation/AppNavigator";
@@ -8,7 +7,6 @@ export default function App() {
   return (
     <NavigationContainer>
       <AppNavigator />
-      <StatusBar style="auto" />
       <Toast />
     </NavigationContainer>
   );
