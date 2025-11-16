c9838a337a1494eb4664a9e9cff2ef6ad44b749e
Bogdan Mihaila <>
2025-11-16 15:46:20 +0200

    Add react-navigation. Create 2 sample screens for login and search screens

--- App.tsx
+++ App.tsx
@@ -1,16 +1,27 @@
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, View } from "react-native";
+import LoginScreen from "./screens/LoginScreen";
+import SearchSchoolScreen from "./screens/SearchSchoolScreen";
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="SearchSchool">
+        <Stack.Screen
+          name="SearchSchool"
+          component={SearchSchoolScreen}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="Login"
+          component={LoginScreen}
+          options={{ headerShown: false }}
+        />
+      </Stack.Navigator>
       <StatusBar style="auto" />
-    </View>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-});
